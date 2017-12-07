9e274b75a294a95adcd32b9c9482c5b0f1ac4917
Marc Kelsey <marc@parallelmade.com>
2017-12-07 14:27:59 -0500

    Reworking 7b

--- 7b.rb
+++ 7b.rb
@@ -22,42 +22,62 @@
 # repeat the whole process
 #
 
-input = File.readlines(__dir__ + '/inputs/7.txt')
-modifiable = File.read(__dir__ + '/inputs/7.txt')
+
+# modify original file to make it easier to work with
+# change (weight) to (self_weight,stack_weight)
+copy = File.read(__dir__ + '/inputs/7.txt')
+copy.gsub! ')', ',0)'
+File.open(__dir__ + '/inputs/7-working.txt', 'w') { |f| f.write copy }
+
 i = 0
 
 while i < 1 do
-  input.each do |line|
+  # find all rows with "nothing on top", then substitute those into
+  # to rows that are holding them
+  scanner = File.readlines(__dir__ + '/inputs/7-working.txt')
+  buffer = File.read(__dir__ + '/inputs/7-working.txt')
+
+  scanner.each do |line|
+    # ignore if "nothing on top", ie no "-> asdf, dsdg" in line
     unless line.match /->/
-      modifiable.sub! line, ''
-      line.sub! '(', ''
-      line.sub! ')', ''
+       # ie, nothing on top
+      buffer.sub! line, ''
+      # update the 'asdlkja' with the weights
       parts = line.split
-      modifiable.sub! parts[0], parts[1]
+      buffer.sub! parts[0], parts[1]
     end
   end
 
-  File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write modifiable }
+  File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write buffer }
 
-  input = File.readlines(__dir__ + '/inputs/mod.txt')
-  input.each do |line|
+  # find all rows which have all their substitutions
+  # then, add up all the stacks and see if they are the same weight
+  # if they are, add up those weights and update the line's 'stack_weight'
+  # if they aren't, figure out which stack is different and return the
+  # stack's 'self_weight' +/- the difference
+  scanner = File.readlines(__dir__ + '/inputs/mod.txt')
+  scanner.each do |line|
+    # get the stuff "on top"
     parts = line.split ' -> '
-    nums = parts[1].split(',').map(&:to_i)
-    unless nums.any? { |el| el == 0 }
-      if nums.min == nums.max
-        total = nums.reduce(&:+)
-        t_and_w = parts[0].split
-        tag = t_and_w[0]
-        weight = t_and_w[1].sub('(', '').sub(')', '')
-        modifiable.sub! line, "#{tag} (#{weight}, #{total})\n"
-      else
-        puts nums.max_by { |i| nums.count(i) }
-        exit
-      end
+    num_pairs = "#{parts[1]}"
+    # skip if line is "unresolved", ie, still has 'aadsfa' in it
+    unless num_pairs.match /a-z/
+      num_pairs = num_pairs.gsub('(', '').sub(")\n", '').split('),').map{ |pair| pair.split(',').map(&:to_i)}
+      puts num_pairs.inspect
+      #   if nums.min == nums.max
+      #     total = nums.reduce(&:+)
+      #     t_and_w = parts[0].split
+      #     tag = t_and_w[0]
+      #     weight = t_and_w[1].sub('(', '').sub(')', '')
+      #     modifiable.sub! line, "#{tag} (#{weight}, #{total})\n"
+      #   else
+      #     puts nums.max_by { |i| nums.count(i) }
+      #     exit
+      #   end
     end
   end
 
-  File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write modifiable }
+  #File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write modifiable }
 
   i += 1
 end
