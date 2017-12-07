0cd5de084e38721691a77891bae2b19e08662450
Marc Kelsey <marc.kelsey@gmail.com>
2017-12-07 18:15:12 -0500

    7 done

--- 7b.rb
+++ 7b.rb
@@ -48,36 +48,34 @@ while i < 1 do
     end
   end
 
-  File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write buffer }
+  File.open(__dir__ + '/inputs/7-working-2.txt', 'w') { |f| f.write buffer }
 
   # find all rows which have all their substitutions
   # then, add up all the stacks and see if they are the same weight
   # if they are, add up those weights and update the line's 'stack_weight'
   # if they aren't, figure out which stack is different and return the
   # stack's 'self_weight' +/- the difference
-  scanner = File.readlines(__dir__ + '/inputs/mod.txt')
+  scanner = File.readlines(__dir__ + '/inputs/7-working-2.txt')
   scanner.each do |line|
     # get the stuff "on top"
     parts = line.split ' -> '
     num_pairs = "#{parts[1]}"
     # skip if line is "unresolved", ie, still has 'aadsfa' in it
-    unless num_pairs.match /a-z/
+    unless num_pairs.match /[a-z]+/
       num_pairs = num_pairs.gsub('(', '').sub(")\n", '').split('),').map{ |pair| pair.split(',').map(&:to_i)}
-      puts num_pairs.inspect
-      #   if nums.min == nums.max
-      #     total = nums.reduce(&:+)
-      #     t_and_w = parts[0].split
-      #     tag = t_and_w[0]
-      #     weight = t_and_w[1].sub('(', '').sub(')', '')
-      #     modifiable.sub! line, "#{tag} (#{weight}, #{total})\n"
-      #   else
-      #     puts nums.max_by { |i| nums.count(i) }
-      #     exit
-      #   end
+      num_sums = num_pairs.map { |pair| pair.reduce(&:+) }
+
+      # if all weight are the same, update the "stack_weight", remove stuff after ->
+      if num_sums.min == num_sums.max
+        total = num_sums.reduce(&:+)
+        t_and_w = parts[0].sub(',0)', ",#{total})")
+        buffer.sub! line, "#{t_and_w}\n"
+      else
+        puts num_pairs.inspect  #.max_by { |i| nums.count(i) }
+        exit
+      end
     end
   end
 
-  #File.open(__dir__ + '/inputs/mod.txt', 'w') { |f| f.write modifiable }
-
-  i += 1
+  File.open(__dir__ + '/inputs/7-working.txt', 'w') { |f| f.write buffer }
 end
