448ea74412e461552c59f4190e549893acd5b1a2
Marc Kelsey <marc.kelsey@gmail.com>
2017-12-12 07:46:59 -0500

    Finish 11

---
+++ 11b.rb
@@ -0,0 +1,52 @@
+input = File.read(__dir__ + '/inputs/11.txt').strip.split(',')
+max = 0
+length = input.length
+
+length.times do |i|
+  puts "#{i} / #{length}" if i % 100 == 0
+  @steps = {
+    'n' => 0,
+    's' => 0,
+    'ne' => 0,
+    'nw' => 0,
+    'se' => 0,
+    'sw' => 0,
+  }
+
+  input.each { |step| @steps[step] += 1 }
+
+  def cancel(a, b)
+    min = [@steps[a], @steps[b]].min
+    @steps[a] -= min
+    @steps[b] -= min
+
+    min
+  end
+
+  done = false
+  old_length = 0
+  new_length = 0
+
+  until done do
+
+    cancel 'n', 's'
+    cancel 'ne', 'sw'
+    cancel 'se', 'nw'
+
+    @steps['nw'] += cancel 'n', 'sw'
+    @steps['sw'] += cancel 's', 'nw'
+    @steps['ne'] += cancel 'n', 'se'
+    @steps['se'] += cancel 's', 'ne'
+    @steps['n'] += cancel 'ne', 'nw'
+    @steps['s'] += cancel 'se', 'sw'
+
+    old_length = new_length
+    new_length = @steps.values.reduce(&:+)
+
+    done = old_length == new_length
+  end
+  input.pop
+  max = new_length if new_length > max
+end
+
+puts max
