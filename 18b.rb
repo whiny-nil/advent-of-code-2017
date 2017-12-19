dde1fc8500e6f071b0aa7fd8093bc4deb8bcfa5a
Marc Kelsey <marc.kelsey@gmail.com>
2017-12-18 19:54:38 -0500

    Finish 18 (fucking wrong operator)

--- 18b.rb
+++ 18b.rb
@@ -1,50 +1,86 @@
-instructions = File.readlines(__dir__ + "/inputs/18.txt").map(&:strip).map(&:split)
-i = 0
-@registers = Hash.new 0
-last_sound = 0
+class Program
+  attr_reader :stopped, :waiting, :messages_sent, :queue
 
-def get_value(x)
-  if x.match /[a-z]/
-    @registers[x]
-  else
-    x.to_i
+  def initialize(id)
+    @id = id
+    @registers = Hash.new 0
+    @registers['p'] = id
+
+    @instructions = File.readlines(__dir__ + "/inputs/18.txt").map(&:strip).map(&:split)
+    #@instructions = File.readlines(__dir__ + "/inputs/18-test.txt").map(&:strip).map(&:split)
+    @i = 0
+
+    @queue = []
+
+    @messages_sent = 0
+
+    @waiting = false
+    @stopped = false
   end
-end
 
+  def receiver=(program)
+    @receiver = program
+  end
 
-loop do
-  offset = 1
-  instruction = instructions[i]
-
-  case instruction[0]
-  when 'snd'
-    value = get_value instruction[1]
-    last_sound = value
-  when 'set'
-    value = get_value instruction[2]
-    @registers[instruction[1]] = value
-  when 'add'
-    value = get_value instruction[2]
-    @registers[instruction[1]] += value
-  when 'mul'
-    value = get_value instruction[2]
-    @registers[instruction[1]] *= value
-  when 'mod'
-    value = get_value instruction[2]
-    @registers[instruction[1]] %= value
-  when 'rcv'
-    value = get_value instruction[1]
-    if value != 0
-      puts last_sound
-      break
-    end
-  when 'jgz'
-    value = get_value instruction[1]
-    if value != 0
-      offset = get_value instruction[2]
+  def get_value(x)
+    if x.match /[a-z]/
+      @registers[x]
+    else
+      x.to_i
     end
   end
 
-  i += offset
-  break if i < 0 || i >= instructions.length
+  def tick
+    return if @stopped
+    offset = 1
+    instruction = @instructions[@i]
+
+    case instruction[0]
+    when 'set'
+      value = get_value instruction[2]
+      @registers[instruction[1]] = value
+    when 'add'
+      value = get_value instruction[2]
+      @registers[instruction[1]] += value
+    when 'mul'
+      value = get_value instruction[2]
+      @registers[instruction[1]] *= value
+    when 'mod'
+      value = get_value instruction[2]
+      @registers[instruction[1]] %= value
+    when 'snd'
+      value = get_value instruction[1]
+      @receiver.queue.push value
+      @messages_sent +=1
+    when 'rcv'
+      if @queue.length > 0
+        value = @queue.shift
+        @registers[instruction[1]] = value
+        @waiting = false
+      else
+        offset = 0
+        @waiting = true
+      end
+    when 'jgz'
+      value = get_value instruction[1]
+      if value > 0
+        offset = get_value instruction[2]
+      end
+    end
+
+    @i += offset
+    @stopped = @i < 0 || @i >= @instructions.length
+  end
 end
+
+p0 = Program.new 0
+p1 = Program.new 1
+p0.receiver = p1
+p1.receiver = p0
+
+until (p0.stopped || p0.waiting) && (p1.stopped || p1.waiting) do
+  p0.tick
+  p1.tick
+end
+
+puts p1.messages_sent
