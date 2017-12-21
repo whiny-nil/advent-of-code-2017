d7871d14e912fd72e1a7f43c0e4f18e7145e0030
Marc Kelsey <marc@parallelmade.com>
2017-12-21 14:58:38 -0500

    WIP

--- 21a.rb
+++ 21a.rb
@@ -1,9 +1,13 @@
 input = File.readlines(__dir__ + '/inputs/21.txt')
 
-patterns = input.map do |row|
+@patterns = input.map do |row|
   row.strip.split(' => ')
     .map { |el| el.split('/') }
     .map { |el| el.map { |le| le.split('') } }
+end
+
+def patterns
+  @patterns
 end
 
 def segment(grid)
@@ -40,9 +44,52 @@ def segment(grid)
   new_grids
 end
 
+def replace(grid)
+  trial_grid = grid
+
+  4.times do
+    selection = patterns.select { |p| p[0] == trial_grid }
+    return selection[0][1] if selection.length > 0
+
+    trial_grid = flip trial_grid
+    selection = patterns.select { |p| p[0] == trial_grid }
+    return selection[0][1] if selection.length > 0
+
+    trial_grid = rotate trial_grid
+  end
+
+  raise StandardError, "Pattern not found! (#{grid.inspect})"
+end
+
+def flip(grid)
+  # horiz flip
+  new_grid = []
+
+  grid.length.times do |i|
+    new_grid << grid[i].reverse
+  end
+
+  new_grid
+end
+
+def rotate(grid)
+  if grid.length == 2
+    new_grid = [[],[]]
+    new_grid[0] = [grid[1][0], grid[0][0]]
+    new_grid[1] = [grid[1][1], grid[0][1]]
+  else
+    new_grid = [[],[],[]]
+    new_grid[0] = [grid[2][0], grid[1][0], grid[0][0]]
+    new_grid[1] = [grid[2][1], grid[1][1], grid[0][1]]
+    new_grid[2] = [grid[2][2], grid[1][2], grid[0][2]]
+  end
+
+  new_grid
+end
+
 def print(grid)
   grid.length.times do |i|
-    puts grid[i-1].join(' ')
+    puts grid[i].join(' ')
   end
   puts
 end
@@ -51,7 +98,7 @@ def count(grid)
   count = 0
 
   grid.length.times do |i|
-    count += grid[i-1].count('#')
+    count += grid[i].count('#')
   end
 
   count
@@ -65,9 +112,10 @@ grid[2] = ['#', '#', '#']
 
 1.times do
   new_grids = segment(grid)
-  #new_grids = new_grids.map { |grid| replace(grid) }
+  new_grids = new_grids.map { |grid| replace(grid) }
+  new_grids.each { |g| print g }
   #grid = assemble(new_grids)
 end
 
-print grid
+#print grid
 puts count grid
