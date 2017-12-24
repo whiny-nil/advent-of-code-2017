5954aadf690a23f8c0a1d9c6e5a61a51a7bd14aa
Marc Kelsey <marc.kelsey@gmail.com>
2017-12-23 21:07:57 -0500

    Finish 23 (with help)

--- 23b2.rb
+++ 23b2.rb
@@ -1,44 +1,39 @@
-@debug = ENV['debug'] == 'true'
+require 'prime'
+@debug = ENV['DEBUG'] == 'true'
 
 b = c = d = e = f = g = h = 0
 
-if @debug
-  b = c = 93
-else
+b = c = 93
+unless  @debug
   b = 109_300
   c = 126_300
 end
 
-mult = 0
+mult = 0 if @debug
 
 loop do
-  f = 1
-  d = 2
-  loop do
-    e = 2
-    loop do
-      g = d
-      g *= e
-      mult += 1
-      g -= b
-      f = 0 if g == 0
-      e += 1
-      g = e
-      g -= b
-      break if g == 0
-    end
-    d += 1
-    g = d
-    g -= b
-    break if g == 0
-  end
+  # f = 1
+  # d = 2
+  # loop do
+  #   e = 2
+  #   loop do
+  #     mult += 1 if @debug
+  #     f = 0 if (d * e - b) == 0
+  #     e += 1
+  #     g = e - b
+  #     break if g == 0
+  #   end
+  #   d += 1
+  #   g = d - b
+  #   break if g == 0
+  # end
 
-  h += 1 if f == 0
-  g = b
-
-  g -= c
+  # h += 1 if f == 0
+  h += 1 unless Prime.prime? b
+  g = b - c
   break if g == 0
   b += 17
 end
 
-puts mult
+puts mult if @debug
+puts h
